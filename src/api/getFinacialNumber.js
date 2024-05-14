aa7bd57deef088801803d6da97145ecc55414d73
gunhee <rjsgml771@naver.com>
2024-05-14 15:15:07 +0900

    update home/getFinacialNumber

--- src/api/getFinacialNumber.js
+++ src/api/getFinacialNumber.js
@@ -3,34 +3,31 @@ import { useEffect, useState } from "react"
 import xml2js from "react-native-xml2js";
 
 
-// 고유번호 xml -> json으로 변환해서 특정 기업 한글로 입력시 그 기업의 고유번호 반환
-const getFinacialNumber = (finacialName) => {
-  const [data, setData] = useState("");
-
-  const fetchData = async() => {
+// 고유번호 xml -> json으로 변환해서 특정 기업 한글로 입력시 그 기업의 고유번호 반환하는 함수입니다.
+const getFinacialNumber = async (finacialName) => {
     try{
       const response = await axios(`http://localhost:8081/assets/CORPCODE.xml`);
 
-      xml2js.parseString(response.data, (error, res) => {
-        if(error){
-          console.log(error);
-        }
-        const json = JSON.parse(JSON.stringify(res));
+      const data = await new Promise((resolve, reject) => 
+        {
+          xml2js.parseString(response.data, (error, res) => {
+          if(error){
+            reject(error);
+          }
+          const json = JSON.parse(JSON.stringify(res));
 
-        const code = json?.result?.list
-        ?.filter(item => item.corp_name[0] === finacialName)
-        .map(item => item.corp_code[0])
-        
-        {code ? setData(code[0]) : setData(null)}
-      })
+          const code = json?.result?.list
+          ?.filter(item => item.corp_name[0] === finacialName)
+          .map(item => item.corp_code[0])
+          
+          resolve(code ? code[0] : null);
+        });
+      });
+      return data;
+
     } catch(error) {
       console.log(error);
     }
-  }
-
-  fetchData();
-
-  return data;
 }
 
 export default getFinacialNumber;
--- src/api/getFinacialStatement.js
+++ src/api/getFinacialStatement.js
@@ -5,28 +5,21 @@ import {key, proxy, day} from "./API";
 const url = `https://opendart.fss.or.kr/api/fnlttSinglAcnt.json?`;
 
 // api -> json : dart api에서 회사의 고유번호로 특정 회사의 특정 년도 재무재표 데이터를 가져옴
-const getFinacialStatement = (company) => {
+const getFinacialStatement = async (company) => {
   const URL = `${url}crtfc_key=${key}&corp_code=${company}&bsns_year=${day}&reprt_code=11011`;
+  try{
+    const response = await axios.get(`${proxy}${URL}`, {
+      headers: {
+        "x-cors-api-key" : `temp_26636be978b259b7e7a203bb4fce455c`,
+      }
+    });
+    const json = await response.data;
+    console.log(json);
 
-  const [data, setData] = useState(null);
-
-  const fetchData = async() => {
-    try{
-      const response = await axios.get(`${proxy}${URL}`, {
-        headers: {
-          "x-cors-api-key" : `temp_69440402647a45224c435b039b0c2354`,
-        }
-      });
-      const json = await response.data;
-      console.log(json.data);
-      setData(json.data);
-    } catch(error){
-      console.error(error);
-    }
-  };
-
-  fetchData();
-
-  return data;
-}
+    return json;
+    
+  } catch(error){
+    console.error(error);
+  }
+};
 export default getFinacialStatement;
